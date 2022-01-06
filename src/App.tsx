6d5cd705ababcddac424f115e32706ab0e69e425
goro-o56 <yamashitagoro2727@gmail.com>
2022-01-06 16:15:01 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { useState } from 'react';
-import './App.css';
-
 export const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <h1>Hello.</h1>
-      <button onClick={() => setCount(count + 1)}>count: {count}</button>
+    <div>
+      <form onSubmit = {(e) => e.preventDefault()}>
+        <input type = "text" value = "" onChange={(e) => e.preventDefault()} />
+        <input
+          type = "submit"
+          value = "追加"
+          onSubmit = {(e) => e.preventDefault()}
+        />
+      </form>
     </div>
   );
 };
